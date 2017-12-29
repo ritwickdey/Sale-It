6b46965c0f9f5a694b567a6b2db338f42c30faa4
Ritwick Dey <ritwickdey@outlook.com>
2017-12-29 12:48:17 +0530

    Seeding Feature Table

---
+++ Migrations/20171229070316_SeedingFeatureTable.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace SaleIt.Migrations
+{
+    public partial class SeedingFeatureTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Features (Name) Values ('Feature1') ");
+            migrationBuilder.Sql("INSERT INTO Features (Name) Values ('Feature2') ");
+            migrationBuilder.Sql("INSERT INTO Features (Name) Values ('Feature3') ");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Features WHERE Name IN ('Feature1','Feature2','Feature3') ");
+        }
+    }
+}
