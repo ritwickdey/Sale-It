394a7512529d27fa8160eeafe1eb525dea733e84
Ritwick Dey <ritwickdey@outlook.com>
2017-12-30 19:17:26 +0530

    PUT: /api/vehicles/{id}

--- Controllers/VehiclesController.cs
+++ Controllers/VehiclesController.cs
@@ -1,7 +1,9 @@
 using System;
+using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using SaleIt.Controllers.Resources;
 using SaleIt.Models;
 using SaleIt.Persistence;
@@ -41,5 +43,30 @@ namespace SaleIt.Controllers
                 return StatusCode(500, new { error = "Something Went Wrong" });
             }
         }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateVehiclesAsync(int id, [FromBody] VehicleResource vehicleResource)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                    return BadRequest(ModelState);
+
+                var vehicle = context.Vehicles.Include(e => e.VehicleFeatures).FirstOrDefault(e => e.Id == id);
+
+                mapper.Map<VehicleResource, Vehicle>(vehicleResource, vehicle);
+                vehicle.LastUpdate = DateTime.Now;
+
+                await context.SaveChangesAsync();
+
+                var result = mapper.Map<Vehicle, VehicleResource>(vehicle);
+                return Ok(result);
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, new { error = "Something Went Wrong" });
+            }
+        }
+
     }
 }
--- Mapping/MappingProfile.cs
+++ Mapping/MappingProfile.cs
@@ -1,5 +1,6 @@
 using System.Linq;
 using AutoMapper;
+using System.Collections.Generic;
 using SaleIt.Controllers.Resources;
 using SaleIt.Models;
 
@@ -17,10 +18,32 @@ namespace SaleIt.Mapping
                 .ForMember(vr => vr.Features, opt =>  opt.MapFrom(v => v.VehicleFeatures.Select(e => e.FeatureId))); 
 
             CreateMap<VehicleResource, Vehicle>()
+                .ForMember(v => v.Id, opt => opt.Ignore())
                 .ForMember(v => v.ContactName, opt => opt.MapFrom(vr => vr.Contact.Name))
                 .ForMember(v => v.ContactEmail, opt => opt.MapFrom(vr => vr.Contact.Email))
                 .ForMember(v => v.ContactPhone, opt => opt.MapFrom(vr => vr.Contact.Phone))
-                .ForMember(v => v.VehicleFeatures, opt => opt.MapFrom(vr => vr.Features.Select(id => new VehicleFeature { FeatureId = id })));
+                .ForMember(v => v.VehicleFeatures, opt => opt.Ignore())
+                .AfterMap((vr,v) => {
+                    //add
+                    foreach(var id in vr.Features) {
+                        if(!v.VehicleFeatures.Any(e => e.FeatureId == id)) {
+                            v.VehicleFeatures.Add(new VehicleFeature { FeatureId = id });   
+                        }
+                    }
+
+                    //remove
+                    var removedFeatures = new List<VehicleFeature>();
+                    foreach(var f in v.VehicleFeatures) {
+                        if(!vr.Features.Any(e => e == f.FeatureId)) {
+                            removedFeatures.Add(f);
+                        }
+                    }
+
+                    removedFeatures.ForEach(e => {
+                        v.VehicleFeatures.Remove(e);
+                    });
+
+                });
         }
 
     }
