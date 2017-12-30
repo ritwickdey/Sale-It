c413b8e5f347d5d8a966c2c93f53439b5ebbc536
Ritwick Dey <ritwickdey@outlook.com>
2017-12-30 17:38:22 +0530

    Mapping.  Vehicle <--> VehicleResource

--- Mapping/MappingProfile.cs
+++ Mapping/MappingProfile.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using AutoMapper;
 using SaleIt.Controllers.Resources;
 using SaleIt.Models;
@@ -11,6 +12,15 @@ namespace SaleIt.Mapping
             CreateMap<Make, MakeResource>();
             CreateMap<Model, ModelResources>();
             CreateMap<Feature, FeatureResource>();
+            CreateMap<Vehicle, VehicleResource>()
+                .ForMember(vr => vr.Contact, opt => opt.MapFrom(v => new ContactResource { Name = v.ContactName, Email = v.ContactEmail, Phone = v.ContactPhone }))
+                .ForMember(vr => vr.Features, opt =>  opt.MapFrom(v => v.VehicleFeatures.Select(e => e.FeatureId))); 
+
+            CreateMap<VehicleResource, Vehicle>()
+                .ForMember(v => v.ContactName, opt => opt.MapFrom(vr => vr.Contact.Name))
+                .ForMember(v => v.ContactEmail, opt => opt.MapFrom(vr => vr.Contact.Email))
+                .ForMember(v => v.ContactPhone, opt => opt.MapFrom(vr => vr.Contact.Phone))
+                .ForMember(v => v.VehicleFeatures, opt => opt.MapFrom(vr => vr.Features.Select(id => new VehicleFeature { FeatureId = id })));
         }
 
     }
