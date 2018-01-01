bc2a53af1e47d04ab2c11f127c7bafe4940056f2
Ritwick Dey <ritwickdey@outlook.com>
2018-01-01 17:39:08 +0530

    Vechicle Models in Angular

---
+++ ClientApp/app/models/vehicle.ts
@@ -0,0 +1,29 @@
+export interface IKeyValuePair {
+    id: number;
+    name: string;
+}
+
+export interface IContact {
+    name: string;
+    phone: string;
+    email?: string;
+}
+
+export interface IVehicle {
+    id: number;
+    model: IKeyValuePair;
+    make: IKeyValuePair;
+    isRegistered: boolean;
+    features: IKeyValuePair[];
+    lastUpdate: string;
+    contact: IContact;
+}
+
+export interface ISaveVehicle {
+    id: number;
+    modelId: number;
+    makeId: number;
+    isRegistered: boolean;
+    features: number[];
+    contact: IContact;
+}
