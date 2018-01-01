b08965aa53f3d203202aff7eb55fb4a0e50308f3
Ritwick Dey <ritwickdey@outlook.com>
2018-01-01 11:56:57 +0530

    Fixed: Missed Import

--- ClientApp/app/services/vehicle.service.ts
+++ ClientApp/app/services/vehicle.service.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs/Observable';
 
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/throw';
 
 @Injectable()
 export class VehicleService {
