2d1fa84dffc40ff1d2ef73b1e1525310bd3a6959
Ritwick Dey <ritwickdey@outlook.com>
2018-01-02 21:37:21 +0530

    typo

--- Core/Models/VehicleQuery.cs
+++ Core/Models/VehicleQuery.cs
@@ -5,6 +5,6 @@ namespace SaleIt.Core.Models
         public int? MakeId { get; set; }
         public int? ModelId { get; set; }
         public string SortBy { get; set; }
-        public bool IsSortAscendring { get; set; }
+        public bool IsSortAscending { get; set; }
     }
 }
