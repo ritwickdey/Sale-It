3ad8db6081cc4aa07cd1e840b0fa10c8de3f5888
Ritwick Dey <ritwickdey@outlook.com>
2018-01-03 20:54:56 +0530

    Migration for Photo

--- Migrations/20180103151948_AddPhoto.Designer.cs
+++ Migrations/20180103151948_AddPhoto.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace SaleIt.Migrations
 {
     [DbContext(typeof(SaleItDbContext))]
-    partial class SaleItDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180103151948_AddPhoto")]
+    partial class AddPhoto
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
