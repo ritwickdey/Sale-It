9cdcaeecbc6902c46da7c7df8277ba9152284957
Ritwick Dey <ritwickdey@outlook.com>
2018-01-03 22:14:46 +0530

    validation added for POST api/vehicles/{vId}/photos endpoint

--- Controllers/PhotoController.cs
+++ Controllers/PhotoController.cs
@@ -1,5 +1,6 @@
 using System;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
 using Microsoft.AspNetCore.Hosting;
@@ -14,6 +15,13 @@ namespace SaleIt.Controllers
     [Route("/api/vehicles/{vehicleId}/photos")]
     public class PhotoController : Controller
     {
+        private readonly int MAX_BYTES = 1024 * 1024;
+        private readonly string[] ACCEPTED_FILE_TYPES = new []
+        {
+            ".jpg",
+            ".jpeg",
+            ".png"
+        };
         private readonly IHostingEnvironment host;
         private readonly IVehicleRepository repository;
         private readonly IUnitOfWork unitOfWork;
@@ -34,11 +42,16 @@ namespace SaleIt.Controllers
         [HttpPost]
         public async Task<IActionResult> Upload(int vehicleId, IFormFile file)
         {
+            if (file == null) return BadRequest(new { error = "Null File" });
+            if (file.Length == 0) return BadRequest(new { error = "Empty File" });
+            if (!ACCEPTED_FILE_TYPES.Any(s => s == Path.GetExtension(file.FileName)))
+                return BadRequest(new { error = $"Unsupported File. Supported files are {String.Join(", ", ACCEPTED_FILE_TYPES).ToUpper()}" });
+            if (file.Length > MAX_BYTES) return BadRequest(new { error = "File Size exceeds 1MB" });
+
             var vehicle = await repository.GetVehicleAsync(vehicleId, includeRelated: false);
 
             if (vehicle == null)
                 return NotFound();
-
 
             var uploadFolderPath = Path.Combine(host.WebRootPath, "uploads");
             if (!Directory.Exists(uploadFolderPath))
