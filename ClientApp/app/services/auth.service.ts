2dea8fb9009018733a5c68fe73ced0bc3795f894
Ritwick Dey <ritwickdey@outlook.com>
2018-01-07 02:39:31 +0530

    Addtional Field & User Profile Fetch & few chanages

--- ClientApp/app/services/auth.service.ts
+++ ClientApp/app/services/auth.service.ts
@@ -1,44 +1,89 @@
 import { Router } from '@angular/router';
 import { Injectable } from '@angular/core';
 import Auth0Lock from 'auth0-lock';
-import { tokenNotExpired } from 'angular2-jwt';
+import { tokenNotExpired, JwtHelper } from 'angular2-jwt';
 
 
 @Injectable()
 export class AuthService {
-  
+
+  profile: any;
+  private roles: any[] = [];
   lock = new Auth0Lock('OzaV1UwlCTaFjSZg9HtgWtanOWHj3ocD', 'ritwickdey.auth0.com', {
     auth: {
-      redirectUrl: `http://localhost:5000/vehicles`,
-      responseType: 'token'
+      redirectUrl: `${window.location.origin}/vehicles`,
+      responseType: 'token',
+      params: {
+        audience: 'https://saleIt.com',
+        scope: 'openid email profile'
+      }
     },
     theme: {
       primaryColor: '#3899D8',
       logo: '/img/icon.png'
-    },  
+    },
+    additionalSignUpFields: [
+      {
+        name: 'name',
+        placeholder: 'Full Name',
+        icon: '/img/details-icon.svg',
+        validator: function (name) {
+          return {
+            valid: new RegExp(/^[a-z,.'-]+\s[a-z, .'-]+$/i).test(name),
+            hint: "Enter a valid Full Name"
+          };
+        }
+      }
+    ],
     autoclose: true,
     autofocus: true,
-    params: {
-      scope: 'openid'
-    },
     languageDictionary: {
       title: 'Log me in'
     }
   });
 
   constructor() {
+    this.profile = JSON.parse(localStorage.getItem('profile')!);
+
+    const token = localStorage.getItem('token');
+    if (token) {
+      const jwtHelper = new JwtHelper();
+      const decodedToken = jwtHelper.decodeToken(token);
+      this.roles = decodedToken['https://SaleIt.com/roles'];
+    }
+
     this.lock.on('authenticated', (authResult) => {
       console.log(authResult);
-      localStorage.setItem('token', authResult.idToken);
+      localStorage.setItem('token', authResult.accessToken);
+
+      const jwtHelper = new JwtHelper();
+      const decodedToken = jwtHelper.decodeToken(authResult.accessToken);
+      this.roles = decodedToken['https://SaleIt.com/roles'];
+
+      this.lock.getUserInfo(authResult.accessToken, (error, profile) => {
+        if (error) throw error;
+
+        console.log(profile);
+        localStorage.setItem('profile', JSON.stringify(profile));
+        this.profile = profile;
+      });
     });
   }
 
-  public login() {
+  login() {
     this.lock.show();
   }
 
-  public logout() {
+  logout() {
     localStorage.removeItem('token');
+    localStorage.removeItem('profile');
+    this.roles = [];
+    this.profile = null;
+  }
+
+  isInRole(rollName) {
+    if(!this.roles) this.roles = [];
+    return this.roles.indexOf(rollName) !== -1;
   }
 
   authenticated() {
