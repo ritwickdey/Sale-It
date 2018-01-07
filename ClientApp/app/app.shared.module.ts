f4722f35e927592e651836d59b56141729802e85
Ritwick Dey <ritwickdey@outlook.com>
2018-01-07 17:52:45 +0530

    fake pie chart

--- ClientApp/app/app.shared.module.ts
+++ ClientApp/app/app.shared.module.ts
@@ -7,6 +7,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
 import { ToastyModule } from 'ng2-toasty';
+import { ChartModule } from 'angular2-chartjs';
 
 import { AppComponent } from './components/app/app.component';
 import { NavMenuComponent } from './components/navmenu/navmenu.component';
@@ -50,6 +51,7 @@ Raven
         HttpModule,
         FormsModule,
         ToastyModule.forRoot(),
+        ChartModule,
         JwtModule.forRoot({
             config: {
                 tokenGetter: () => localStorage.getItem('token')!
