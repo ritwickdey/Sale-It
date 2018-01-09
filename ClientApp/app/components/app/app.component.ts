31d7636f16361f131edbd9fe26b78497894bdec8
Ritwick Dey <ritwickdey@outlook.com>
2018-01-09 13:12:15 +0530

    Admin account prompt added & promt color changed

--- ClientApp/app/components/app/app.component.ts
+++ ClientApp/app/components/app/app.component.ts
@@ -1,9 +1,39 @@
-import { Component } from '@angular/core';
+import { NgZone } from '@angular/core';
+import { ToastyService } from 'ng2-toasty';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'app',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+    constructor(
+        private ngZone: NgZone,
+        private toastyService: ToastyService) {
+
+    }
+
+    ngOnInit() {
+       
+           setTimeout(() => {
+            this.toastyService.warning({
+                title: 'Moderator',
+                msg: 'Account: example@example.com \n pass: 12345678',
+                timeout: 5000,
+                theme: 'bootstrap',
+            }) 
+           }, 6000);
+
+           setTimeout(() => {
+            this.toastyService.info({
+                title: 'Note',
+                msg: 'Only Admin/Moderator can add/edit/delete Vehichles',
+                timeout: 5000,
+                theme: 'bootstrap',
+            }) 
+           }, 3000);
+      
+    }
 }
